fbac3813e1fd18a62b9776b91a571f06b73d515f
Otto Quaglio <otto.q@gmx.com>
2023-12-04 11:04:29 +0800

    refactor: restructure Dockerfile directories
    - Project folder layout reorganized for clarity.
    - Dockerfiles sorted into app, database, messaging, etc.
    - Improves ease of navigation and management.

---
+++ messaging/solace/dual-node/README.md
@@ -0,0 +1,11 @@
+# Dual Node Solace Brokers
+
+Adapted from https://github.com/SolaceLabs/solace-single-docker-compose
+
+## Start
+
+docker compose -f docker-compose.yml up -d
+
+## Stop
+
+docker compose -f docker-compose.yml down
---
+++ messaging/solace/dual-node/create_topic_and_queue.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+# This script automates the creation of a queue and a topic subscription in Solace PubSub+.
+# It utilizes the Solace Element Management Protocol (SEMP) API to send configuration commands
+# to the Solace broker. The script first creates a queue and then subscribes a topic to that queue.
+#
+# Variables:
+# - ADMIN_USER: Username for the admin access to the Solace broker.
+# - ADMIN_PASSWORD: Password for the admin access.
+# - SOLACE_URL: The URL where the Solace broker SEMP interface is accessible.
+# - QUEUE_NAME: The name of the queue to be created.
+# - TOPIC_NAME: The name of the topic to subscribe to the queue.
+# - ENCODED_QUEUE_NAME: The URL-encoded name of the queue to use in the API request.
+
+# Admin credentials
+ADMIN_USER="admin"
+ADMIN_PASSWORD="admin"
+SOLACE_URL="http://localhost:8080"  # Adjust if using a different port or host
+
+# Queue and topic names
+QUEUE_NAME="q/minaus/waio/sf/cam/trackmeasurement/recorded/v1"
+TOPIC_NAME="t/minaus/waio/sf/cam/trackmeasurement/recorded/v1"
+
+# URL-encode the queue name to handle any special characters (like slashes)
+ENCODED_QUEUE_NAME=$(echo "$QUEUE_NAME" | sed 's:/:%2F:g')
+
+#echo "Encoded Queue Name: $ENCODED_QUEUE_NAME"
+
+# Create a queue using the SEMP API
+curl -X POST "$SOLACE_URL/SEMP/v2/config/msgVpns/default/queues" \
+     -u "$ADMIN_USER:$ADMIN_PASSWORD" \
+     -H "Content-Type: application/json" \
+     -d '{
+           "queueName": "'$QUEUE_NAME'",
+           "accessType": "exclusive",
+           "egressEnabled": true,
+           "ingressEnabled": true
+         }'
+
+# Create a topic subscription for the queue using the SEMP API
+curl -X POST "$SOLACE_URL/SEMP/v2/config/msgVpns/default/queues/$ENCODED_QUEUE_NAME/subscriptions" \
+     -u "$ADMIN_USER:$ADMIN_PASSWORD" \
+     -H "Content-Type: application/json" \
+     -d '{
+           "subscriptionTopic": "'$TOPIC_NAME'"
+         }'
